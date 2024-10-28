2c2835d086c0a8c1ebff5abbd46a3708e3fb64b1
SajjadRADev <sajjadra.dev@gmail.com>
2024-10-28 15:03:27 +0330

    feat: implement change topic order and remove topic methods

--- src/controllers/v1/topics.ts
+++ src/controllers/v1/topics.ts
@@ -46,6 +46,58 @@ export const update = async (req: Request<RequestParamsWithID, {}, UpdateTopicSc
     }
 };
 
-export const updateOrder = async (req: Request, res: Response, next: NextFunction) => {};
+export const updateOrder = async (req: Request<RequestParamsWithID, {}, UpdateTopicOrderSchemaType>, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const { from, to } = req.body;
 
-export const remove = async (req: Request, res: Response, next: NextFunction) => {};
+        const fromTopic = await TopicModel.findOne({ _id: id, order: from });
+
+        if (!fromTopic) {
+            throw new NotFoundException("from topic not found");
+        }
+
+        const toTopic = await TopicModel.findOne({ order: to, course: fromTopic.course });
+
+        if (!toTopic) {
+            throw new NotFoundException("to topic not found");
+        }
+
+        const fromTopicOrder = fromTopic.order;
+
+        fromTopic.order = toTopic.order;
+        toTopic.order = fromTopicOrder;
+
+        await fromTopic.save();
+        await toTopic.save();
+
+        SuccessResponse(res, 200, { message: "order changed successfully" });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const remove = async (req: Request<RequestParamsWithID>, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+
+        const topic = await TopicModel.findByIdAndDelete(id);
+
+        // TODO: handle side effects
+
+        if (!topic) {
+            throw new NotFoundException("topic not found");
+        }
+
+        await TopicModel.updateMany(
+            { course: topic.course, order: { $gt: topic.order } },
+            {
+                $inc: { order: -1 },
+            }
+        );
+
+        SuccessResponse(res, 200, { topic });
+    } catch (err) {
+        next(err);
+    }
+};
--- src/routes/v1/topics.ts
+++ src/routes/v1/topics.ts
@@ -10,7 +10,7 @@ import roleGuard from "@/middlewares/roleGuard";
 const router = express.Router();
 
 router.post("/", auth, roleGuard(ROLES.MANAGER), validator("body", CreateTopicSchema), create);
-router.route("/:id").patch(auth, roleGuard(ROLES.MANAGER), validator("body", UpdateTopicSchema), update).delete(remove);
+router.route("/:id").patch(auth, roleGuard(ROLES.MANAGER), validator("body", UpdateTopicSchema), update).delete(auth, roleGuard(ROLES.MANAGER), remove);
 router.patch("/:id/order", auth, roleGuard(ROLES.MANAGER), validator("body", UpdateTopicOrderSchema), updateOrder);
 
 export default router;
