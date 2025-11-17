764a6c9186dd6fc3306de57566a7b716340f4fa9
VoidZero0402 <voidzero0204@gmail.com>
2025-11-17 18:15:40 +0330

    fix: better regex for phone number

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -5,7 +5,7 @@ export const SignupShcema = z.object({
     phone: z
         .string({ required_error: "phone is a required field" })
         .trim()
-        .regex(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g, { message: "phone is not valid" }),
+        .regex(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,10}$/g, { message: "phone is not valid" }),
     fullname: z.string({ required_error: "fullname is required" }).min(3, { message: "fullname should be more than or equal 3 character" }).max(255, { message: "fullname should be less than or equal 255 character" }),
     password: z.string({ required_error: "password is required" }).min(7, { message: "password should be more than or equal 7 character" }).max(255, { message: "password should be less than or equal 255 character" }),
     otp: z.string({ required_error: "otp is a required field" }).regex(/^[0-9]{5}$/, { message: "otp should be a 5 digits number" }),
