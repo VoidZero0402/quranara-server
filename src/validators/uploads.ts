2f7ec9367ca371154573887b76bbca5fbbaf0d8d
VoidZero0402 <voidzero0204@gmail.com>
2025-12-04 18:56:17 +0330

    fix: extend uploads file types

--- src/validators/uploads.ts
+++ src/validators/uploads.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const types = ["image/jpeg", "image/png", "image/webp", "audio/mpeg", "audio/wav", "application/pdf", "application/x-compressed", "application/x-zip-compressed"] as const;
+const types = ["image/jpeg", "image/png", "image/webp", "audio/mpeg", "audio/wav", "application/pdf", "application/zip", "application/x-compressed", "application/x-zip-compressed", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"] as const;
 
 export const GetPreSignedUrlQuerySchema = z.object({
     filename: z.string({ required_error: "filename is required" }).min(1, { message: "filename should not be empty" }),
